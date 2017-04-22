df0d1f1a3b4c7ed854f8a362682e215599d83d50
£lye$a <hi@za.ax>
2017-04-22 18:50:24 +0200

    Create coolload.js

---
+++ cdn/ezXSS/js/coolload.js
@@ -0,0 +1,25 @@
+$("body").on('click','a', function() {
+  // Make sure it is in the same window
+  if($(this).attr("target") != "_blank") {
+
+    // Change opacity of main to 0.2
+    $("section").css("opacity","0.2");
+    // Prevent the default event (redirect)
+    event.preventDefault();
+    // Get the href of the clicked link
+    var href = $(this).attr("href");
+
+    // Get the sourcecode of the href
+    $.get(href, function(source) {
+      // Get the main html from source
+      var main = source.substring(source.indexOf('<body>') + 6, source.indexOf('</body>'));
+
+      // Append this html to the main
+      $("body").html(main);
+      // Change URL to the href
+      window.history.pushState(href, 'Hi, I\'m Elyesa.', href);
+      // Change opacity back to 1
+      $("section").css("opacity","1");
+    });
+  }
+});
